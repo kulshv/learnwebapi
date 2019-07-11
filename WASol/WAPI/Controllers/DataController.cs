5467ed52f877721413ebdf6701394cdbd949dc7d
kulshv <vikas28978@gmail.com>
2019-07-11 23:09:03 +0530

    Add data controller

---
+++ WASol/WAPI/Controllers/DataController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WAPI.Controllers
+{
+    public class DataController : ApiController
+    {
+        [HttpGet]
+        [Route ("/api/data/alldata")]
+        public IHttpActionResult get()
+        {
+            return Ok("Hi From Get");
+        }
+
+    }
+}
